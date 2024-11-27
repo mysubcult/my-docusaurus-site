02ffb49e3d041aaffebffaad2995cf604346caea
Artyom Gorin <mysubcult1@gmail.com>
2024-11-27 18:01:21 +0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,8 +54,8 @@ const config: Config = {
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/mysubcult/my-docusaurus-site',
-          label: 'GitHub',
+          href: 'https://смартдиаг.рф',
+          label: 'Официальный сайт',
           position: 'right',
         },
       ],
