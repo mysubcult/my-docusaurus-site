26b38d9e3d8eb7f716380815945a6738db5e629e
Artyom Gorin <mysubcult1@gmail.com>
2024-11-27 22:49:41 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Войти 📚 
           </Link>
         </div>
       </div>
