b32f39ed6b195d0b744e8187d778d19942e5d54d
Artyom Gorin <mysubcult1@gmail.com>
2024-11-27 23:22:43 +0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -43,7 +43,7 @@ const config: Config = {
       title: 'SmartDiag',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
