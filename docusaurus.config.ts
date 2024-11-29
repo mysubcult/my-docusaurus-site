884119e4eb21ffd007fb6bdab9fb126e60931636
Artyom Gorin <mysubcult1@gmail.com>
2024-11-30 00:01:21 +0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -127,37 +127,18 @@ const config: Config = {
   } satisfies Preset.ThemeConfig,
   scripts: [
     {
-      id: "lhc-widget-script",
-      strategy: "afterInteractive",
-      dangerouslySetInnerHTML: {
-        __html: `
-          var LHC_API = LHC_API||{};
-          LHC_API.args = {
-            mode: 'widget',
-            lhc_base_url: 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',
-            wheight: 450,
-            wwidth: 350,
-            pheight: 520,
-            pwidth: 500,
-            domain: 'docs.xn--80aajcuv3afm.xn--p1ai',
-            leaveamessage: true,
-            department: ["1"],
-            theme: "9",
-            check_messages: false,
-            lang: 'rus/'
-          };
-          (function() {
-            var po = document.createElement('script');
-            po.type = 'text/javascript';
-            po.setAttribute("crossorigin", "anonymous");
-            po.async = true;
-            var date = new Date();
-            po.src = 'https://xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(po, s);
-          })();
-        `,
-      },
+      src: '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js',
+      async: true,
+      crossorigin: 'anonymous',
+      innerHTML: `
+        var LHC_API = LHC_API||{};
+        LHC_API.args = {mode:'widget',lhc_base_url:'//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'docs.смартдиаг.рф',department:["1"],check_messages:false};
+        (function() {
+          var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+          var date = new Date();po.src = '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+        })();
+      `,
     },
   ],
 };
