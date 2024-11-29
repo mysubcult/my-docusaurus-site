52365c9cf83b084ad558dd720204c74cab37a04d
Artyom Gorin <mysubcult1@gmail.com>
2024-11-29 17:50:55 +0400

    Delete src/js directory

--- src/js/custom.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const navbarLinks = document.querySelectorAll('.navbar__link');
-  navbarLinks.forEach(link => {
-    link.setAttribute('target', '_self');
-  });
-});
