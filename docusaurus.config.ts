6f8dc73920cf1999bdb19f72f07c91f6ab31b0a7
Artyom Gorin <mysubcult1@gmail.com>
2024-11-30 00:05:31 +0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -125,22 +125,6 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
-  scripts: [
-    {
-      src: '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js',
-      async: true,
-      crossorigin: 'anonymous',
-      innerHTML: `
-        var LHC_API = LHC_API||{};
-        LHC_API.args = {mode:'widget',lhc_base_url:'//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'docs.xn--80aajcuv3afm.xn--p1ai',department:["1"],check_messages:false};
-        (function() {
-          var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-          var date = new Date();po.src = '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-        })();
-      `,
-    },
-  ],
 };
 
 export default config;
