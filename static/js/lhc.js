435bbb00dcee24975548abd4e55c3c57c9acbbb1
Artyom Gorin <mysubcult1@gmail.com>
2024-12-05 14:23:42 +0400

    Update lhc.js

--- static/js/lhc.js
+++ static/js/lhc.js
@@ -1,5 +1,5 @@
 var LHC_API = LHC_API||{};
-LHC_API.args = {mode:'widget',lhc_base_url:'//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'docs.смартдиаг.рф',department:["1"],check_messages:false};
+LHC_API.args = {mode:'widget',lhc_base_url:'//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,domain:'docs.xn--80aajcuv3afm.xn--p1ai',department:["1"],check_messages:false};
 (function() {
 var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
 var date = new Date();po.src = '//xn----7sbabnedajkp5ap8aokkew.xn--p1ai/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
